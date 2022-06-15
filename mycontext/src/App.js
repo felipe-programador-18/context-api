1f18ee32c0584db18015f716c914a3e8bb27ffda
Felipe <machadofelipe2016@outlook.com>
2022-06-15 18:18:10 -0300

    c0-practice and little bit about context

---
+++ mycontext/src/App.js
@@ -0,0 +1,28 @@
+
+import React, {useContext} from 'react'
+import { createContext } from 'react';
+import "./App.css"
+
+//practice little bit about contextapi!!
+const manageAll  = createContext()
+
+const Other = () => {
+  // This case const save
+  const save = useContext(manageAll)
+  return <div> teste {save} </div>
+
+}
+
+function App() {
+  const another = "programmer Felipe 18"
+  return (
+    <manageAll.Provider value={another} >
+      <div className="App">
+        App
+       <Other/>
+       </div>
+    </manageAll.Provider>
+  );
+}
+
+export default App;
