b2176408ee60253902e08aaff7266686452dffcf
Felipe <machadofelipe2016@outlook.com>
2022-07-10 14:28:41 -0300

    c22-adding more code

--- mycontext/src/AnotherButton.js
+++ mycontext/src/AnotherButton.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { useMessage } from './hoock/useContext'
+
 
 const AnotheButton = ({TradePhase}) => {
    const AddingPhase = ['life better ?','improve life', 'grop up be away']
-
-
+    
+   const {count, setCount} = useMessage()
+   console.log('teste count',count)
+   
+   const HandGropup = () => {
+      setCount(count+2)
+   }
+    
   return(<>
-     
+         
      <button  onClick={()=> TradePhase(AddingPhase[0]) } >1</button>
      <button onClick={() => TradePhase(AddingPhase[1])} >2</button>
      <button onClick={() => TradePhase(AddingPhase[2])}  >3</button>
-  
+     <div>
+      <h1>Adding more code to thing about create hoock personalites</h1>
+
+      <h4>My Counter is {count} </h4>
+      <button onClick={HandGropup} >+ two</button>
+     </div>
   </>)
 }
 
